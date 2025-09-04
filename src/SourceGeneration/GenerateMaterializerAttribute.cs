28692b9fe6a2f840eb28532de78bab5bddcc0b11
ZilverZtream <zilverztream@gmail.com>
2025-09-04 18:48:43 +0200

    Enhance source generation attributes and query generator

--- src/SourceGeneration/GenerateMaterializerAttribute.cs
+++ src/SourceGeneration/GenerateMaterializerAttribute.cs
@@ -2,7 +2,13 @@ using System;
 
 namespace nORM.SourceGeneration
 {
-    [AttributeUsage(AttributeTargets.Class)]
+    /// <summary>
+    /// Marks an entity type for which a materializer should be generated at
+    /// compile time. The associated source generator will produce a method that
+    /// can populate an instance of the decorated type directly from a
+    /// <see cref="System.Data.Common.DbDataReader"/>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public sealed class GenerateMaterializerAttribute : Attribute
     {
     }
