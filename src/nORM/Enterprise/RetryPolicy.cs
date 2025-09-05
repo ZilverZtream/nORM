b9e3446ae98bf066863b6a6d0456fbc074764e0f
Dennis Östling <zilverztream@gmail.com>
2025-09-05 14:12:54 +0200

    Add robust retry logic

--- src/nORM/Enterprise/RetryPolicy.cs
+++ src/nORM/Enterprise/RetryPolicy.cs
@@ -10,6 +10,7 @@ namespace nORM.Enterprise
     {
         public int MaxRetries { get; set; } = 3;
         public TimeSpan BaseDelay { get; set; } = TimeSpan.FromSeconds(1);
-        public Func<DbException, bool> ShouldRetry { get; set; } = ex => ex is SqlException sqlEx && sqlEx.Number is 4060 or 40197 or 40501 or 40613 or 49918 or 49919 or 49920 or 1205;
+        public Func<DbException, bool> ShouldRetry { get; set; } = ex =>
+            ex is SqlException sqlEx && sqlEx.Number is 4060 or 40197 or 40501 or 40613 or 49918 or 49919 or 49920 or 1205 or 1222;
     }
 }
