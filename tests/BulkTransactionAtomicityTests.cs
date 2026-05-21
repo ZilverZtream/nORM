92435af64a294acde4902325c4271784897bf182
Dennis Östling <zilverztream@gmail.com>
2026-05-21 17:20:22 +0200

    Make PostgreSQL bulk batches atomic

--- tests/BulkTransactionAtomicityTests.cs
+++ tests/BulkTransactionAtomicityTests.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
+using Npgsql;
 using nORM.Core;
 using nORM.Providers;
 using Xunit;
@@ -19,6 +22,15 @@ public class BulkTransactionAtomicityTests
     {
         [Key]
         public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+
+    [Table("PgBulkAtomicRows")]
+    private class PgBulkAtomicRow
+    {
+        [Key]
+        public int Id { get; set; }
+
         public string Name { get; set; } = string.Empty;
     }
 
@@ -124,5 +136,42 @@ public class BulkTransactionAtomicityTests
 
         var count = ctx.Query<Item>().Count();
         Assert.Equal(0, count);
+    }
+
+    [Fact]
+    public async Task Postgres_BulkUpdate_BatchFailure_RollsBackEarlierBatches_Live()
+    {
+        var cs = LiveProviderEnvironment.GetConnectionString("postgres");
+        if (string.IsNullOrEmpty(cs)) return;
+
+        await using var cn = new NpgsqlConnection(cs);
+        await cn.OpenAsync();
+
+        await using (var setup = cn.CreateCommand())
+        {
+            setup.CommandText = """
+                DROP TABLE IF EXISTS "PgBulkAtomicRows";
+                CREATE TABLE "PgBulkAtomicRows" ("Id" INTEGER PRIMARY KEY, "Name" TEXT NOT NULL);
+                INSERT INTO "PgBulkAtomicRows" ("Id", "Name")
+                SELECT i, 'original' FROM generate_series(1, 11) AS s(i);
+                """;
+            await setup.ExecuteNonQueryAsync();
+        }
+
+        using var ctx = new DbContext(cn, new PostgresProvider(new SqliteParameterFactory()));
+        var updates = Enumerable.Range(1, 11)
+            .Select(i => new PgBulkAtomicRow
+            {
+                Id = i,
+                Name = i == 11 ? null! : "updated"
+            })
+            .ToArray();
+
+        await Assert.ThrowsAnyAsync<Exception>(() => ctx.BulkUpdateAsync(updates));
+
+        await using var probe = cn.CreateCommand();
+        probe.CommandText = "SELECT COUNT(*) FROM \"PgBulkAtomicRows\" WHERE \"Name\" = 'updated'";
+        var leakedUpdates = Convert.ToInt32(await probe.ExecuteScalarAsync());
+        Assert.Equal(0, leakedUpdates);
     }
 }
