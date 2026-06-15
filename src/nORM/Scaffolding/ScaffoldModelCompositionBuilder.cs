94e62539e312cc3218ad20567dc1278aa7104c34
Dennis Östling <zilverztream@gmail.com>
2026-06-15 04:43:10 +0200

    Split scaffold model composition steps

--- src/nORM/Scaffolding/ScaffoldModelCompositionBuilder.cs
+++ src/nORM/Scaffolding/ScaffoldModelCompositionBuilder.cs
@@ -10,42 +10,16 @@ namespace nORM.Scaffolding
         public static ScaffoldModelComposition Build(ScaffoldModelDiscoveryResult discovery)
         {
             var featureConfigurations = discovery.FeatureConfigurations;
-            var manyToManyJoins = ScaffoldRelationshipAdapter.BuildManyToManyJoins(
-                discovery.ForeignKeys,
-                discovery.Tables,
-                discovery.EntityByTable,
-                discovery.ColumnPropertiesByTable,
-                discovery.PrimaryKeyColumnsByTable,
-                discovery.IdentityColumnsByTable,
-                featureConfigurations.ComputedColumnsByTable,
-                discovery.Indexes,
-                discovery.NonNullableColumnsByTable,
-                featureConfigurations.ProviderOwnedWriteBlockedTableKeys,
-                discovery.MemberNamesByTable);
-            var manyToManyJoinTableKeys = manyToManyJoins
-                .Select(static join => join.JoinTableKey)
-                .ToHashSet(StringComparer.OrdinalIgnoreCase);
-            var relationships = ScaffoldRelationshipAdapter.BuildRelationships(
-                discovery.ForeignKeys
-                    .Where(fk => !manyToManyJoinTableKeys.Contains(TableKey(fk.DependentSchema, fk.DependentTable)))
-                    .ToArray(),
-                discovery.EntityByTable,
-                discovery.ColumnPropertiesByTable,
-                discovery.PrimaryKeyColumnsByTable,
-                discovery.Indexes,
-                discovery.NonNullableColumnsByTable,
-                discovery.MemberNamesByTable);
+            var manyToManyJoins = BuildManyToManyJoins(discovery, featureConfigurations);
+            var manyToManyJoinTableKeys = BuildManyToManyJoinTableKeys(manyToManyJoins);
+            var relationships = BuildNonJoinRelationships(discovery, manyToManyJoinTableKeys);
             var compositePrimaryKeys = ScaffoldRelationshipAdapter.BuildPrimaryKeyConfigurations(
                 discovery.EntityByTable,
                 discovery.ColumnPropertiesByTable,
                 discovery.PrimaryKeyColumnsByTable,
                 discovery.PrimaryKeyConstraintNamesByTable,
                 manyToManyJoinTableKeys);
-            var defaultValueConfigurations = ScaffoldFeatureConfigurationAdapter.BuildDefaultValueConfigurations(
-                discovery.EntityByTable,
-                discovery.ColumnPropertiesByTable,
-                featureConfigurations.DefaultValuesByTable,
-                featureConfigurations.DefaultConstraintNamesByTable);
+            var defaultValueConfigurations = BuildDefaultValueConfigurations(discovery, featureConfigurations);
 
             RestoreGeneratedManyToManyUnsupportedFeatures(
                 discovery.UnsupportedFeatures,
@@ -57,31 +31,92 @@ namespace nORM.Scaffolding
                 manyToManyJoinTableKeys,
                 relationships,
                 compositePrimaryKeys,
-                defaultValueConfigurations
-                    .Where(config => !manyToManyJoinTableKeys.Contains(config.TableKey))
-                    .ToArray(),
-                featureConfigurations.CheckConstraints
-                    .Where(config => !manyToManyJoinTableKeys.Contains(config.TableKey))
-                    .ToArray(),
-                featureConfigurations.ComputedColumnConfigurations
-                    .Where(config => !manyToManyJoinTableKeys.Contains(config.TableKey))
-                    .ToArray(),
-                featureConfigurations.ExpressionIndexConfigurations
-                    .Where(config => !manyToManyJoinTableKeys.Contains(config.TableKey))
-                    .ToArray(),
-                featureConfigurations.CollationConfigurations
-                    .Where(config => !manyToManyJoinTableKeys.Contains(config.TableKey))
-                    .ToArray(),
-                featureConfigurations.IdentityOptionConfigurations
-                    .Where(config => !manyToManyJoinTableKeys.Contains(config.TableKey))
-                    .ToArray(),
-                featureConfigurations.PrecisionConfigurations
-                    .Where(config => !manyToManyJoinTableKeys.Contains(config.TableKey))
-                    .ToArray(),
-                featureConfigurations.ColumnFacetConfigurations
-                    .Where(config => !manyToManyJoinTableKeys.Contains(config.TableKey))
-                    .ToArray());
+                ExcludeJoinTableConfigurations(
+                    defaultValueConfigurations,
+                    manyToManyJoinTableKeys,
+                    static config => config.TableKey),
+                ExcludeJoinTableConfigurations(
+                    featureConfigurations.CheckConstraints,
+                    manyToManyJoinTableKeys,
+                    static config => config.TableKey),
+                ExcludeJoinTableConfigurations(
+                    featureConfigurations.ComputedColumnConfigurations,
+                    manyToManyJoinTableKeys,
+                    static config => config.TableKey),
+                ExcludeJoinTableConfigurations(
+                    featureConfigurations.ExpressionIndexConfigurations,
+                    manyToManyJoinTableKeys,
+                    static config => config.TableKey),
+                ExcludeJoinTableConfigurations(
+                    featureConfigurations.CollationConfigurations,
+                    manyToManyJoinTableKeys,
+                    static config => config.TableKey),
+                ExcludeJoinTableConfigurations(
+                    featureConfigurations.IdentityOptionConfigurations,
+                    manyToManyJoinTableKeys,
+                    static config => config.TableKey),
+                ExcludeJoinTableConfigurations(
+                    featureConfigurations.PrecisionConfigurations,
+                    manyToManyJoinTableKeys,
+                    static config => config.TableKey),
+                ExcludeJoinTableConfigurations(
+                    featureConfigurations.ColumnFacetConfigurations,
+                    manyToManyJoinTableKeys,
+                    static config => config.TableKey));
         }
+
+        private static IReadOnlyList<DatabaseScaffolder.ScaffoldManyToManyJoin> BuildManyToManyJoins(
+            ScaffoldModelDiscoveryResult discovery,
+            DatabaseScaffolder.ScaffoldFeatureConfigurations featureConfigurations)
+            => ScaffoldRelationshipAdapter.BuildManyToManyJoins(
+                discovery.ForeignKeys,
+                discovery.Tables,
+                discovery.EntityByTable,
+                discovery.ColumnPropertiesByTable,
+                discovery.PrimaryKeyColumnsByTable,
+                discovery.IdentityColumnsByTable,
+                featureConfigurations.ComputedColumnsByTable,
+                discovery.Indexes,
+                discovery.NonNullableColumnsByTable,
+                featureConfigurations.ProviderOwnedWriteBlockedTableKeys,
+                discovery.MemberNamesByTable);
+
+        private static IReadOnlySet<string> BuildManyToManyJoinTableKeys(
+            IEnumerable<DatabaseScaffolder.ScaffoldManyToManyJoin> manyToManyJoins)
+            => manyToManyJoins
+                .Select(static join => join.JoinTableKey)
+                .ToHashSet(StringComparer.OrdinalIgnoreCase);
+
+        private static IReadOnlyList<DatabaseScaffolder.ScaffoldRelationship> BuildNonJoinRelationships(
+            ScaffoldModelDiscoveryResult discovery,
+            IReadOnlySet<string> manyToManyJoinTableKeys)
+            => ScaffoldRelationshipAdapter.BuildRelationships(
+                discovery.ForeignKeys
+                    .Where(fk => !manyToManyJoinTableKeys.Contains(TableKey(fk.DependentSchema, fk.DependentTable)))
+                    .ToArray(),
+                discovery.EntityByTable,
+                discovery.ColumnPropertiesByTable,
+                discovery.PrimaryKeyColumnsByTable,
+                discovery.Indexes,
+                discovery.NonNullableColumnsByTable,
+                discovery.MemberNamesByTable);
+
+        private static IReadOnlyList<DatabaseScaffolder.ScaffoldDefaultValueConfiguration> BuildDefaultValueConfigurations(
+            ScaffoldModelDiscoveryResult discovery,
+            DatabaseScaffolder.ScaffoldFeatureConfigurations featureConfigurations)
+            => ScaffoldFeatureConfigurationAdapter.BuildDefaultValueConfigurations(
+                discovery.EntityByTable,
+                discovery.ColumnPropertiesByTable,
+                featureConfigurations.DefaultValuesByTable,
+                featureConfigurations.DefaultConstraintNamesByTable);
+
+        private static T[] ExcludeJoinTableConfigurations<T>(
+            IEnumerable<T> configurations,
+            IReadOnlySet<string> manyToManyJoinTableKeys,
+            Func<T, string> getTableKey)
+            => configurations
+                .Where(config => !manyToManyJoinTableKeys.Contains(getTableKey(config)))
+                .ToArray();
 
         public static void RestoreGeneratedManyToManyUnsupportedFeatures(
             List<DatabaseScaffolder.ScaffoldUnsupportedFeature> unsupportedFeatures,
